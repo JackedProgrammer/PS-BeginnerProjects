ebfc6ced2f68d699e9dd6003895df3705cc5d050
JackedProgrammer <82956386+JackedProgrammer@users.noreply.github.com>
2022-03-31 22:38:13 -0400

    Create LogCleaner.ps1

---
+++ LogCleaner.ps1
@@ -0,0 +1,10 @@
+
+$DirectoryListFilePath="C:\Users\Administrator.HYPV2016L\Desktop\PS-BeginnerProjects\LogDirectories.csv"
+
+$DirectoryList=Import-Csv -Path $DirectoryListFilePath
+
+foreach($Directory in $DirectoryList){
+    Get-ChildItem -Path $Directory.DirectoryPath -Filter "$($Directory.FileName)*" `
+    | Where-Object LastWriteTime -lt $(Get-Date).AddDays(-$Directory.KeepForDays) `
+    | Remove-Item -Confirm:$false -Force -WhatIf
+}
