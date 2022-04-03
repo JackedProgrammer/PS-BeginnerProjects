cb2203d729b80323c5cc3d1da2b6545e9646a220
JackedProgrammer <82956386+JackedProgrammer@users.noreply.github.com>
2022-04-03 14:15:30 -0400

    Update LogCleaner.ps1

--- LogCleaner.ps1
+++ LogCleaner.ps1
@@ -6,5 +6,5 @@ $DirectoryList=Import-Csv -Path $DirectoryListFilePath
 foreach($Directory in $DirectoryList){
     Get-ChildItem -Path $Directory.DirectoryPath -Filter "$($Directory.FileName)*" `
     | Where-Object LastWriteTime -lt $(Get-Date).AddDays(-$Directory.KeepForDays) `
-    | Remove-Item -Confirm:$false -Force -WhatIf
+    | Remove-Item -Confirm:$false -Force
 }
